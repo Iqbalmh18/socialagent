63d3a4699deec4b44328b1a0e99a579dcefb87aa
Moh Iqbal Hidayat <41185294+Iqbalmh18@users.noreply.github.com>
2025-01-29 11:23:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='socialagent',
-    version='0.1',
-    description='socialagent is a Python module designed for generating user agents specific to social media platforms under Meta (formerly Facebook).',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Moh Iqbal Hidayat',
-    author_email='iqbalmh18.dev@email.com',
-    url='https://github.com/iqbalmh18/socialagent',
-    packages=['socialagent'],
+    name="socialagent",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[],
+    author="Moh Iqbal Hidayat",
+    author_email="iqbalmh18.dev@gmail.com",
+    description="socialagent is a Python module designed for generating user agents specific to social media platforms under Meta (formerly Facebook).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iqbalmh18/socialagent",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.6",
 )
